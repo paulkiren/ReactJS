4298e8c2eaa963678f9f66f512cc81d2347b685c
Kiren Paul <kiren.paul@mobme.in>
2018-01-03 12:20:55 +0530

    handle click from board side

--- hello-world/src/index.js
+++ hello-world/src/index.js
@@ -18,14 +18,40 @@ import "./index.css";
 // }
 
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null
+    };
+  }
   render() {
-    return <button className="square" onClick={() => alert('click')}>{this.props.value}</button>;
+    return (
+      <button className="square" onClick={() => this.props.onClick()}>
+        {this.state.value}
+      </button>
+    );
   }
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null)
+    };
+  }
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
