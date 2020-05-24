b808ab12c59cde0cd1ba194c37025fe29466cfa4
PaulKiren <kirenpaul@gmail.com>
2020-05-24 12:57:32 +0700

    numpad is created

--- todo_hooks_exp/README.md
+++ todo_hooks_exp/README.md
@@ -66,3 +66,7 @@ This section has moved here: https://facebook.github.io/create-react-app/docs/de
 ### `yarn build` fails to minify
 
 This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+
+
+we are going to build an calculator application just as in the android pixel phones.
+
--- todo_hooks_exp/src/App.js
+++ todo_hooks_exp/src/App.js
@@ -7,13 +7,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      
+
        
       </header>
       <Calculator></Calculator>
+
+     
     </div> 
   );
 }
